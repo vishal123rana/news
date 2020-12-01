886d5dc275db1ede7de58b365be615ac33a10ace
vishal rana <vishalrana030201998@gmail.com>
2020-12-01 17:49:18 +0530

    initial Commit Setup

--- src/main/java/com/backend_news/news/Controller/NewsController.java
+++ src/main/java/com/backend_news/news/Controller/NewsController.java
@@ -1,4 +1,12 @@
 package com.backend_news.news.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NewsController {
+    @GetMapping("/home")
+    public String home(){
+        return "Hello World";
+    }
 }
--- src/main/java/com/backend_news/news/NewsApplication.java
+++ src/main/java/com/backend_news/news/NewsApplication.java
@@ -3,11 +3,10 @@ package com.backend_news.news;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class NewsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NewsApplication.class, args);
 	}
-
 }
